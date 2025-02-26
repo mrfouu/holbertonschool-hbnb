e41aab2491c238580d265bb7e014d02d78dfb133
Bree-Coding <brendapollagba@gmail.com>
2025-02-26 17:19:31 +0100

    refactor: simplify Review model attributes and property methods

--- Part_2/app/models/review.py
+++ Part_2/app/models/review.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
 
 from app.models.base_model import BaseModel
-import re
-from .user import User
-from .place import Place
 
 
 class Review(BaseModel):
-    def __init__(self, place, user, text, rating):
+    def __init__(self, text, place, user, rating=None):
         """
         Initialize a new Review instance.
         
         Args:
+            text (str): The text content of the review.
             place_id (str): The ID of the place being reviewed.
             user_id (str): The ID of the user writing the review.
-            text (str): The text content of the review.
-            rating (int): The rating of the review.
+            rating (int, optional): The rating of the review. Defaults to None.
         """
-        super().__init__()
-        self._place = place
-        self._user = user
-        self._text = text
-        self._rating = rating
+        self.text = text
+        self.place = place
+        self.user = user
+        self.rating = rating
 
     @property
-    def place(self):
+    def place_id(self):
         """
         Get the place_id of the review.
         
         Returns:
             str: The ID of the place being reviewed.
         """
-        return self._place
+        return self.place_id
 
-    @place.setter
-    def place(self, value):
+    @place_id.setter
+    def place_id(self, value):
         """
         Set the place_id of the review.
         
@@ -46,20 +42,20 @@ class Review(BaseModel):
         """
         if value is None or not isinstance(value, str):
             raise TypeError('place_id must be a non-empty string')
-        self._place= value
+        self.place_id = value
 
     @property
-    def user(self):
+    def user_id(self):
         """
         Get the user_id of the review.
         
         Returns:
             str: The ID of the user writing the review.
         """
-        return self._user
+        return self.user_id
 
-    @user.setter
-    def user(self, value):
+    @user_id.setter
+    def user_id(self, value):
         """
         Set the user_id of the review.
         
@@ -71,7 +67,7 @@ class Review(BaseModel):
         """
         if value is None or not isinstance(value, str):
             raise TypeError('user_id must be a non-empty string')
-        self._user = value
+        self.user_id = value
 
     @property
     def text(self):
@@ -81,7 +77,7 @@ class Review(BaseModel):
         Returns:
             str: The text content of the review.
         """
-        return self._text
+        return self .text
 
     @text.setter
     def text(self, value):
@@ -96,7 +92,7 @@ class Review(BaseModel):
         """
         if value is None or not isinstance(value, str):
             raise TypeError('text must be a non-empty string')
-        self._text = value
+        self.text = value
 
     @property
     def rating(self):
@@ -106,7 +102,7 @@ class Review(BaseModel):
         Returns:
             int: The rating of the review.
         """
-        return self._rating
+        return self.rating
 
     @rating.setter
     def rating(self, value):
@@ -124,4 +120,4 @@ class Review(BaseModel):
             raise TypeError('rating must be an integer')
         if value < 0 or value > 5:
             raise ValueError('rating must be between 0 and 5')
-        self._rating = value
+        self.rating = value
