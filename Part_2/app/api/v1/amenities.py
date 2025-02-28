c32d56d7f5ec877913062bef7dcb7ba96613e367
knarta <charquen10@gmail.com>
2025-02-28 12:10:05 +0100

    "Fixed styling errors according to pycodestyle"

--- Part_2/app/api/v1/amenities.py
+++ Part_2/app/api/v1/amenities.py
@@ -21,7 +21,7 @@ class AmenityList(Resource):
         data_ameneties = api.payload
         if not data_ameneties:
             return {'message': 'Invalid input data'}, 400
-        
+
         new_amenities = facade.create_amenity(data_ameneties)
         return {'id': new_amenities.id, 'name': new_amenities.name}, 201
 
@@ -29,10 +29,11 @@ class AmenityList(Resource):
     def get(self):
         """Retrieve a list of all amenities"""
         ameneties = facade.get_all_amenities()
-        return {'ameneties':[{'id': ameneties.id,
-                              'name': ameneties.name
-                              }for ameneties in ameneties]}, 200
-    
+        return {'ameneties': [{'id': ameneties.id,
+                               'name': ameneties.name}
+                              for ameneties in ameneties
+                              }], 200
+
 
 @api.route('/<amenity_id>')
 class AmenityResource(Resource):
@@ -52,13 +53,12 @@ class AmenityResource(Resource):
     def put(self, amenity_id):
         """Update an amenity's information"""
         amenity_data = api.payload
-        
+
         if not amenity_data:
             return {'message': 'Invalid input data'}, 400
-        
+
         updated_amenity = facade.update_amenity(amenity_id, amenity_data)
         if not updated_amenity:
             return {'message': 'Amenity not found'}, 404
-        
+
         return {'id': updated_amenity.id, 'name': updated_amenity.name}, 200
-    
