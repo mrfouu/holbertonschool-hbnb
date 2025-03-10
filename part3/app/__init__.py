0fd6c08da5d56d73086513308b52a19c4f2e6a51
Bree-Coding <brendapollagba@gmail.com>
2025-03-10 19:51:42 +0100

    refactor: update create_app to accept configuration class and clean up imports

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,24 +1,19 @@
 from flask import Flask 
-from flask_restx import Api, Resource 
+from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.services.facade import HBnBFacade
-
-facade = HBnBFacade()
-
-def create_app():
-    app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
+import config
 
-def create_app():
+facade = HBnBFacade()
+
+def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
 
-    # Placeholder for API namespaces (endpoints will be added later)
-    
-    # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
