ebe5a1048bfbee791ff6916825f06822cdc7dae8
knarta <charquen10@gmail.com>
2025-03-11 12:36:18 +0100

    refactor: clean up imports and remove merge conflict remnants in app initialization

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,28 +1,13 @@
 from flask import Flask 
 from flask_restx import Api
 from app.services.facade import HBnBFacade
-<<<<<<< HEAD
 import config
-=======
+
 from flask_jwt_extended import JWTManager
->>>>>>> chacha
+
 
 facade = HBnBFacade()
 jwt = JWTManager()
-
-
-
-<<<<<<< HEAD
-=======
-def create_app():
-    app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
-from app.api.v1.places import api as places_ns
-from app.api.v1.users import api as auth_ns
-from app.api.v1.users import api as users_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.amenities import api as amenities_ns
->>>>>>> chacha
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
