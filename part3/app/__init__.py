8f3d41825665c99fdd190c229914d9eda85ccc2c
knarta <charquen10@gmail.com>
2025-03-11 12:59:59 +0100

    add authentication namespace import in app initialization

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -14,6 +14,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
 
     from app.api.v1.users import api as users_ns
+    from app.api.v1.users import api as auth_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
     from app.api.v1.amenities import api as amenities_ns
