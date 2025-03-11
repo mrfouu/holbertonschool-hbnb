0232acbec66920f36cc3ef89720d5f49ec928c83
knarta <charquen10@gmail.com>
2025-03-11 12:41:59 +0100

    fix: resolve merge conflict in app initialization

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -23,11 +23,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
-<<<<<<< HEAD
-
-=======
     
     jwt.init_app(app)
     
->>>>>>> origin
     return app
