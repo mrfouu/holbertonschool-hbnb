e09c601cfea31811fe8598e85bae3ab457812b0d
knarta <charquen10@gmail.com>
2025-03-11 12:31:17 +0100

    add JWT authentication setup and user authentication namespace

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask 
-from flask_restx import Api, Resource 
-from app.api.v1.users import api as users_ns
+from flask_restx import Api
 from app.services.facade import HBnBFacade
+from flask_jwt_extended import JWTManager
 
 facade = HBnBFacade()
+jwt = JWTManager()
+
+
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 from app.api.v1.places import api as places_ns
+from app.api.v1.users import api as auth_ns
+from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 
@@ -20,8 +25,11 @@ def create_app():
     
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    
+    jwt.init_app(app)
     
     return app
