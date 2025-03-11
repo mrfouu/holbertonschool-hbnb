021288906e63c6047274131e5fe8f7c2b10c480a
knarta <charquen10@gmail.com>
2025-03-11 17:26:29 +0100

    feat: initialize SQLAlchemy in app for database integration

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_restx import Api
 from app.services.facade import HBnBFacade
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 
 
 facade = HBnBFacade()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -24,6 +26,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     
+    db.init_app(app)
     jwt.init_app(app)
+    
     
     return app
