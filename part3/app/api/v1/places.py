aaf57073665af042bb9d9edb1e43fb8c1f1ad520
knarta <charquen10@gmail.com>
2025-03-14 20:23:48 +0100

    refactor: improve place data handling and response structure in places.py

--- part3/app/api/v1/places.py
+++ part3/app/api/v1/places.py
@@ -2,6 +2,7 @@ from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.services.facade import HBnBFacade
 
+
 api = Namespace('places', description='Place operations')
 
 facade = HBnBFacade()
@@ -43,13 +44,13 @@ class PlaceList(Resource):
     @jwt_required()
     def post(self):
         """Register a new place"""
-
-        place_data = api.payload
-        if not place_data:
-            return {'message': 'Invalid input data'}, 400
         
         current_user = get_jwt_identity()
+        place_data = api.payload
         place_data['owner_id'] = current_user['id']
+
+        if not place_data:
+            return {'message': 'Invalid input data'}, 400
 
         try:
             new_place = facade.create_place(place_data)
@@ -64,26 +65,32 @@ class PlaceList(Resource):
                 "latitude": new_place.latitude,
                 "longitude": new_place.longitude,
                 "owner_id": new_place.owner_id,
-                "amenities": new_place._amenities}, 201
+                "amenities": new_place.amenities}, 201
 
     @api.response(200, 'List of places retrieved successfully')
     def get(self):
         """Retrieve a list of all places"""
         places = facade.get_all_places()
-        return {'id': places.id,
-                'title': places.title,
-                'description': places.description,
-                'price': places.price,
-                'latitude': places.latitude,
-                'longitude': places.longitude,
-                'owner_id': places.owner_id,
-                'amenities': places.amenities}, 200
-
+        return { 
+            'places': [
+                {
+                    'id': places.id,
+                    'title': places.title,
+                    'description': places.description,
+                    'price': places.price,
+                    'latitude': places.latitude,
+                    'longitude': places.longitude,
+                    'owner_id': places.owner_id,
+                    'amenities': places.amenities
+                } for places in places
+            ]
+        }, 200
 
 @api.route('/<place_id>')
 class PlaceResource(Resource):
     @api.response(200, 'Place details retrieved successfully')
     @api.response(404, 'Place not found')
+    @jwt_required()
     def get(self, place_id):
         """Get place details by ID"""
         # Placeholder for the logic to retrieve a place by ID,
@@ -99,7 +106,7 @@ class PlaceResource(Resource):
                 "latitude": place_data.latitude,
                 "longitude": place_data.longitude,
                 "owner_id": place_data.owner_id,
-                "amenities": place_data._amenities}, 200
+                "amenities": place_data.amenities}, 200
 
     @api.expect(place_model)
     @api.response(200, 'Place updated successfully')
@@ -136,4 +143,4 @@ class PlaceResource(Resource):
                 "latitude": updated_place.latitude,
                 "longitude": updated_place.longitude,
                 "owner_id": updated_place.owner_id,
-                "amenities": updated_place._amenities}, 200
+                "amenities": updated_place.amenities}, 200
