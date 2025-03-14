aa25c6bbb84c2184da0786094407704113542009
knarta <charquen10@gmail.com>
2025-03-14 20:24:26 +0100

    refactor: update amenity model to use String for IDs and improve relationship definition

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -4,18 +4,17 @@ import uuid
 
 # Table d'association pour la relation Many-to-Many
 place_amenity = db.Table('place_amenity',
-    db.Column('place_id', db.Integer, db.ForeignKey('places.id'), primary_key=True),
-    db.Column('amenity_id', db.Integer, db.ForeignKey('amenities.id'), primary_key=True)
+    db.Column('place_id', db.String(36), db.ForeignKey('places.id'), primary_key=True),
+    db.Column('amenity_id', db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
 )
 
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
 
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(50), nullable=False)
 
     # Relation Many-to-Many avec Place
-    places = db.relationship('Place', secondary=place_amenity, backref='amenities', lazy=True)
+    places = db.relationship('Place', secondary='place_amenity', back_populates='amenities', lazy=True)
 
     def validate(self):
         if not self.name:
